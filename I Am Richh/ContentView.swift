79e1b18a4bab6296fd281e275c06d9a2a8db360d
Famil <frostgamingrespawn@gmail.com>
2020-09-29 21:19:14 +0200

    Initial Commit

---
+++ I Am Richh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Richh
+//
+//  Created by Famil Samadli on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I Am Rich")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
